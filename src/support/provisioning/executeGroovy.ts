3354c2b9e8b857415aa2fed8b99f00526422cb59
Francois Gerthoffert <f.gerthoffert@gmail.com>
2022-02-24 10:35:20 -0500

    Added the ability to pass a custom server to provisioning commands

--- src/support/provisioning/executeGroovy.ts
+++ src/support/provisioning/executeGroovy.ts
@@ -9,16 +9,28 @@ declare global {
     namespace Cypress {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         interface Chainable<Subject> {
-            executeGroovy(scriptFile: string, replacements?: { [key: string]: string }): Chainable<any>
+            executeGroovy(scriptFile: string, jahiaServer: JahiaServer, replacements?: { [key: string]: string }): Chainable<any>
         }
     }
 }
 
-export const executeGroovy = function (scriptFile: string, replacements?: { [key: string]: string }): void {
+type JahiaServer = {
+    url: string;
+    username: string;
+    password: string
+}
+
+const serverDefaults = {
+    url: Cypress.config().baseUrl,
+    username: 'root',
+    password: Cypress.env('SUPER_USER_PASSWORD')
+}
+
+export const executeGroovy = function (scriptFile: string, jahiaServer: JahiaServer = serverDefaults, replacements?: { [key: string]: string }): void {
     cy.runProvisioningScript({
         fileContent: '- executeScript: "' + scriptFile + '"',
         type: 'application/yaml'
-    }, [{
+    }, jahiaServer, [{
         fileName: scriptFile,
         replacements,
         type: 'text/plain'
