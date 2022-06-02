4356dd20efa2605b9bc6eebe5209ca49fcd7a4c0
Cedric Mailleux <cmailleux@jahia.com>
2022-06-02 15:31:53 -0400

    Add collapsible.ts to handle Collapsible component from Moonstone (#7)
    * Add collapsible.ts to handle Collapsible component from Moonstone
    
    * Collapse/Expand only if needed

---
+++ src/page-object/moonstone/collapsible.ts
@@ -0,0 +1,24 @@
+import {BaseComponent} from "../baseComponent"
+
+
+export class Collapsible extends BaseComponent {
+    static defaultSelector = '.moonstone-collapsible'
+
+    collapse(): Collapsible {
+        this.get().children('div').then(($child) => {
+            if ($child.hasClass('moonstone-collapsible_content_expanded')) {
+                this.get().find('.moonstone-collapsible_button').click()
+            }
+        })
+        return this
+    }
+
+    expand(): Collapsible {
+        this.get().children('div').then(($child) => {
+            if ($child.hasClass('moonstone-collapsible_content_collapsed')) {
+                this.get().find('.moonstone-collapsible_button').click().scrollIntoView()
+            }
+        })
+        return this
+    }
+}
