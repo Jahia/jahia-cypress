02a92548efc659bf7c7c41a2282e3481d69d6ecb
Thomas Draier <tdraier@jahia.com>
2023-10-04 11:54:59 +0200

    Better handling of default values

--- src/utils/JCRHelper.ts
+++ src/utils/JCRHelper.ts
@@ -49,8 +49,8 @@ export const getNodeByPath = (path: string, properties?: string[], language?: st
             path: path,
             properties: properties,
             language: language,
-            childrenTypes: childrenTypes,
-            workspace: workspace
+            childrenTypes: childrenTypes || [],
+            workspace: workspace || 'EDIT'
         },
         queryFile: 'graphql/jcr/query/getNodeByPath.graphql'
     });
