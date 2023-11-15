ab9cdd267ed5949408ca0ab0958ceca276d265ef
Thomas Draier <tdraier@jahia.com>
2023-11-15 17:32:40 +0100

    Add submenu support (#55)

--- src/page-object/moonstone/menu.ts
+++ src/page-object/moonstone/menu.ts
@@ -1,19 +1,29 @@
 import {BaseComponent} from '../baseComponent';
+import {getComponentByRole} from '../utils';
 
 export class Menu extends BaseComponent {
     static defaultSelector = '.moonstone-menu:not(.moonstone-hidden)'
 
+    submenu(item: string, menu: string): Menu {
+        this.get().find('.moonstone-menuItem').contains(item).should('be.visible').realHover();
+        return getComponentByRole(Menu, menu);
+    }
+
+    shouldHaveItem(item: string):void {
+        this.get().find('.moonstone-menuItem').contains(item).should('be.visible');
+    }
+
+    shouldNotHaveItem(item: string):void {
+        this.get().find('.moonstone-menuItem').contains(item).should('not.exist');
+    }
+
     select(item: string): Menu {
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(500);
-        this.get().find('.moonstone-menuItem').should('contain', item).contains(item).trigger('click');
+        this.get().find('.moonstone-menuItem').contains(item).should('be.visible').trigger('click');
         return this;
     }
 
     selectByRole(item: string): Menu {
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(500);
-        this.get().find(`.moonstone-menuItem[data-sel-role="${item}"]`).trigger('click');
+        this.get().find(`.moonstone-menuItem[data-sel-role="${item}"]`).should('be.visible').trigger('click');
         return this;
     }
 }
