f7e9f5e54c5f123f07efcfaf47d7db6caf8de90a
Thomas Draier <tdraier@jahia.com>
2023-11-16 09:46:09 +0100

    Add scroll in menu (#56)

--- src/page-object/moonstone/menu.ts
+++ src/page-object/moonstone/menu.ts
@@ -5,12 +5,13 @@ export class Menu extends BaseComponent {
     static defaultSelector = '.moonstone-menu:not(.moonstone-hidden)'
 
     submenu(item: string, menu: string): Menu {
-        this.get().find('.moonstone-menuItem').contains(item).should('be.visible').realHover();
+        this.get().find('.moonstone-menuItem').contains(item).scrollIntoView().should('be.visible');
+        this.get().find('.moonstone-menuItem').contains(item).realHover();
         return getComponentByRole(Menu, menu);
     }
 
     shouldHaveItem(item: string):void {
-        this.get().find('.moonstone-menuItem').contains(item).should('be.visible');
+        this.get().find('.moonstone-menuItem').contains(item).scrollIntoView().should('be.visible');
     }
 
     shouldNotHaveItem(item: string):void {
@@ -18,12 +19,14 @@ export class Menu extends BaseComponent {
     }
 
     select(item: string): Menu {
-        this.get().find('.moonstone-menuItem').contains(item).should('be.visible').trigger('click');
+        this.get().find('.moonstone-menuItem').contains(item).scrollIntoView().should('be.visible');
+        this.get().find('.moonstone-menuItem').contains(item).trigger('click');
         return this;
     }
 
     selectByRole(item: string): Menu {
-        this.get().find(`.moonstone-menuItem[data-sel-role="${item}"]`).should('be.visible').trigger('click');
+        this.get().find(`.moonstone-menuItem[data-sel-role="${item}"]`).scrollIntoView().should('be.visible');
+        this.get().find(`.moonstone-menuItem[data-sel-role="${item}"]`).trigger('click');
         return this;
     }
 }
