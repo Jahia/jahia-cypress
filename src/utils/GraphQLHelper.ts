6d02eb872f28218f3d1344fa813d9541a883f041
Francois G <fgerthoffert@jahia.com>
2024-02-07 19:17:11 +0100

    Cleaned up the code and followed closely the GraphQL spec

--- src/utils/GraphQLHelper.ts
+++ src/utils/GraphQLHelper.ts
@@ -1,28 +1,27 @@
-/* eslint max-depth: ["error", 8] */
+/* eslint max-depth: ["error", 5] */
 
-interface GraphQLType {
-    name: string
-    nodePath: string[]
-    kind: string
-    description: string
+interface GraphQLField {
+    name: string;
+}
+
+interface GraphQLDescription {
+    name: string; // Name of the element in the schema
+    description: string; // Description of the element in the schema
+    schemaType: string; // Type of the element in the schema according to the GraphQL spec
+    schemaNode: GraphQLField; // The actual node fetched from the schema
+    nodePath: string[] // The path to the node in the schema
 }
 
 // This returns a flat list of all children fields and args for a given GraphQL node
 // This list can then be used in Cypress tests to look for missing descriptions
 export const getDescriptions = (rootNode: string): Cypress.Chainable => {
     cy.log('Starting analysis from GraphQL node: ' + rootNode);
-    return execIntrospection(rootNode, [], [rootNode]).then(types => {
-        return types;
+    return execIntrospection(rootNode, [], [rootNode]).then(descriptions => {
+        return descriptions;
     });
 };
 
-const execIntrospection = (typeName: string, types: GraphQLType[], nodePath): Cypress.Chainable => {
-    // Do not execute introspection on default type names (String, Int, ...)
-    // only process a node once for a given name
-    if (['String', 'Int', 'Boolean', 'Long'].includes(typeName) || types.find(t => t.name === typeName) !== undefined) {
-        return;
-    }
-
+const execIntrospection = (typeName: string, descriptions: GraphQLDescription[], nodePath): Cypress.Chainable => {
     return cy.apollo({
         variables: {
             typeName: typeName
@@ -35,62 +34,119 @@ const execIntrospection = (typeName: string, types: GraphQLType[], nodePath): Cy
         }
 
         if (responseDataType) {
-            types.push({
-                nodePath,
-                ...responseDataType
-            });
-            if (responseDataType.kind === 'OBJECT') {
-                const fields = responseDataType.fields;
-                if (fields) {
-                    // Looping througth all fields to add to the type array:
-                    // - nodes arguments
-                    // - leaf nodes (whose content cannot be fetched by deeper introspection query)
-                    for (const field of responseDataType.fields) {
-                        if (field.args) {
-                            for (const arg of field.args) {
-                                const argPath = [...nodePath, field.name, arg.name];
-                                // Only adding an arg to the types array if it's not already present
-                                if (types.find(t => t.nodePath.join('/') === argPath.join('/')) === undefined) {
-                                    types.push({
-                                        nodePath: argPath,
-                                        kind: 'ARGUMENT',
-                                        ...arg
-                                    });
-                                }
-                            }
-                        }
+            // This array will be populated with types identified in the introspection query
+            // These will then be further introspected to get their children fields and args
+            const fetchSubTypes: {typeName: string, atPath: string[]}[] = [];
 
-                        if (
-                            (field.type.ofType !== null && ['String', 'Int', 'Boolean', 'Long'].includes(field.type.ofType.name)) ||
-                            (field.type !== null && ['String', 'Int', 'Boolean', 'Long'].includes(field.type.name))
-                        ) {
-                            const fieldPath = [...nodePath, field.name];
-                            if (types.find(t => t.nodePath.join('/') === fieldPath.join('/')) === undefined) {
-                                types.push({
-                                    nodePath: fieldPath,
-                                    kind: field.type.kind,
-                                    ...field
-                                });
-                            }
+            descriptions.push({
+                name: responseDataType.name,
+                description: responseDataType.description,
+                schemaType: '__Type',
+                schemaNode: responseDataType,
+                nodePath
+            });
+
+            // The following exploration of the object follows precisely the Graphql Introspection
+            // spec available at https://github.com/graphql/graphql-spec/blob/main/spec/Section%204%20--%20Introspection.md
+            if (responseDataType.fields) {
+                for (const graphqlField of responseDataType.fields) {
+                    const fieldPath = [...nodePath, responseDataType.name, graphqlField.name];
+                    descriptions.push({
+                        name: graphqlField.name,
+                        description: graphqlField.description,
+                        schemaType: '__Field',
+                        schemaNode: graphqlField,
+                        nodePath: fieldPath
+                    });
+                    fetchSubTypes.push({typeName: graphqlField.type.name, atPath: fieldPath});
+
+                    if (graphqlField.args) {
+                        for (const graphQLInputValue of graphqlField.args) {
+                            const inputValuePath = [...fieldPath, graphQLInputValue.name];
+                            descriptions.push({
+                                name: graphQLInputValue.name,
+                                description: graphQLInputValue.description,
+                                schemaType: '__InputValue',
+                                schemaNode: graphQLInputValue,
+                                nodePath: inputValuePath
+                            });
+                            fetchSubTypes.push({typeName: graphQLInputValue.type.name, atPath: inputValuePath});
                         }
                     }
-
-                    // Fetch more data via a deeper introspection query whenever possible
-                    return Cypress.Promise.each(fields, (field: {type: {kind: string; name: string; ofType: { name: string}}}) => {
-                        if (['OBJECT', 'NON_NULL', 'SCALAR', 'LIST'].includes(field.type.kind)) {
-                            let childTypeName = field.type.name;
-                            if (field.type.kind === 'NON_NULL' || field.type.kind === 'LIST') {
-                                childTypeName = field.type.ofType.name;
-                            }
-
-                            return execIntrospection(childTypeName, types, [...nodePath, childTypeName]);
-                        }
-                    }).then(() => types); // Return types after all recursive calls have completed
                 }
             }
-        }
 
-        return types;
+            if (responseDataType.interfaces) {
+                for (const graphQLInterfaceType of responseDataType.interfaces) {
+                    const fieldPath = [...nodePath, responseDataType.name, graphQLInterfaceType.name];
+                    descriptions.push({
+                        name: graphQLInterfaceType.name,
+                        description: graphQLInterfaceType.description,
+                        schemaType: '__Type',
+                        schemaNode: graphQLInterfaceType,
+                        nodePath: fieldPath
+                    });
+                    fetchSubTypes.push({typeName: graphQLInterfaceType.name, atPath: fieldPath});
+                }
+            }
+
+            if (responseDataType.possibleTypes) {
+                for (const graphQLType of responseDataType.possibleTypes) {
+                    const fieldPath = [...nodePath, responseDataType.name, graphQLType.name];
+                    descriptions.push({
+                        name: graphQLType.name,
+                        description: graphQLType.description,
+                        schemaType: '__Type',
+                        schemaNode: graphQLType,
+                        nodePath: fieldPath
+                    });
+                    fetchSubTypes.push({typeName: graphQLType.name, atPath: fieldPath});
+                }
+            }
+
+            if (responseDataType.enumValues) {
+                for (const graphQLEnumValue of responseDataType.enumValues) {
+                    const enumPath = [...nodePath, responseDataType.name, graphQLEnumValue.name];
+                    descriptions.push({
+                        name: graphQLEnumValue.name,
+                        description: graphQLEnumValue.description,
+                        schemaType: '__EnumValue',
+                        schemaNode: graphQLEnumValue,
+                        nodePath: enumPath
+                    });
+                }
+            }
+
+            if (responseDataType.inputFields) {
+                for (const graphQLInputValue of responseDataType.inputFields) {
+                    const inputValuePath = [...nodePath, responseDataType.name, graphQLInputValue.name];
+                    descriptions.push({
+                        name: graphQLInputValue.name,
+                        description: graphQLInputValue.description,
+                        schemaType: '__InputValue',
+                        schemaNode: graphQLInputValue,
+                        nodePath: inputValuePath
+                    });
+                    fetchSubTypes.push({typeName: graphQLInputValue.type.name, atPath: inputValuePath});
+                }
+            }
+
+            if (responseDataType.ofType) {
+                fetchSubTypes.push({typeName: responseDataType.ofType.name, atPath: nodePath});
+            }
+
+            const uniqueSubTypes = fetchSubTypes
+                // Filter out duplicate types to ensure we don't introspect the same type multiple times
+                .filter((obj, index) => fetchSubTypes.findIndex(item => item.typeName === obj.typeName) === index)
+                // Filter out types that have a name of null (e.g. List of non-null types)
+                .filter(subtype => subtype.typeName !== null)
+                // Remove types that might have already been introspected
+                .filter(subtype => descriptions.find(d => d.schemaType === '__Type' && d.name === subtype.typeName) === undefined);
+
+            return Cypress.Promise.each(uniqueSubTypes, subType => {
+                return execIntrospection(subType.typeName, descriptions, [...subType.atPath, subType.typeName]);
+            }).then(() => descriptions); // Return descriptions after all recursive calls have completed
+        }
     });
 };
 
