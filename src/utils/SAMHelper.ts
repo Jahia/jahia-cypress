f003f2ca12b570baae1dec2ee432a61b8f560c43
Kevan <kevan@jahia.com>
2024-10-17 10:56:35 +0200

    QA-15287: provide generic method for SAM helper and upgrade to yarn 4

--- src/utils/SAMHelper.ts
+++ src/utils/SAMHelper.ts
@@ -1,36 +1,67 @@
-/*
-    When Jahia is starting or performing provisioning operations
-    it is expected for the SAM probe to alternate beween GREEN, YELLOW and RED statuses.
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Chainable = Cypress.Chainable
 
-    The primary use of this method is to wait until a Jahia platform stabilizes after a startup or
-    provisioning operation.
-
-    Instead of waiting only for one occurence of a GREEN status, this function will wait until the a
-    GREEN status was returned a number of consecutive times (greenMatchCount).
-*/
-export const waitUntilSAMStatusGreen = (severity = 'MEDIUM', timeout = 60000, interval = 500, greenMatchCount = 10) : void => {
-    let greenCount = 0;
-    let lastGraphqlResponse = {};
-    cy.waitUntil(() =>
-        cy.apollo({
+/**
+ * Simple health check query
+ * @param severity the severity of the health check, default is MEDIUM
+ * @param probeHealthFilter the filter for the health check probes, default is GREEN
+ */
+export const healthCheck = (severity = 'MEDIUM', probeHealthFilter = 'GREEN'): Chainable<any> => {
+    return cy
+        .apollo({
             fetchPolicy: 'no-cache',
             queryFile: 'graphql/sam/healthStatus.graphql',
             variables: {
-                severity: severity
+                severity,
+                probeHealthFilter
             }
-        }).then(result => {
-            const healthStatus = result?.data?.admin?.jahia?.healthCheck?.status;
-            lastGraphqlResponse = result?.data?.admin?.jahia?.healthCheck;
+        })
+        .then((response: any) => {
+            return response?.data?.admin?.jahia?.healthCheck;
+        });
+};
+
+/**
+ * Wait until the health check returns the expected health
+ * @param expectedHealth the expected health status
+ * @param severity the severity of the health check, default is MEDIUM
+ * @param probeHealthFilter the filter for the health check probes, default is GREEN
+ * @param timeout the timeout in milliseconds, default is 60000
+ * @param interval the interval in milliseconds, default is 500
+ * @param statusMatchCount the number of consecutive status matches before the waitUntil resolves, default is 1
+ */
+export const waitUntilSAMStatus = (expectedHealth: string, severity = 'MEDIUM', probeHealthFilter = 'GREEN', timeout = 60000, interval = 500, statusMatchCount = 1) => {
+    let statusCount = 0;
+    let lastGraphqlResponse = {};
+    cy.waitUntil(() =>
+        healthCheck(severity, probeHealthFilter).then(result => {
+            lastGraphqlResponse = result;
+            const healthStatus = result?.status;
             if (healthStatus) {
-                greenCount = healthStatus.health === 'GREEN' ? greenCount + 1 : 0;
-                return greenCount >= greenMatchCount;
+                statusCount = healthStatus.health === expectedHealth ? statusCount + 1 : 0;
+                return statusCount >= statusMatchCount;
             }
         }),
     {
-        errorMsg: `Timeout waiting for SAM to be green for severity: ${severity}. Last GraphQL response: ${JSON.stringify(lastGraphqlResponse)}`,
+        errorMsg: `Timeout waiting for SAM to be ${expectedHealth} for severity: ${severity} and probeHealthFilter: ${probeHealthFilter}. Last GraphQL response: ${JSON.stringify(lastGraphqlResponse)}`,
         timeout: timeout,
         verbose: true,
         interval: interval
     });
+};
+
+/**
+ When Jahia is starting or performing provisioning operations
+ it is expected for the SAM probe to alternate beween GREEN, YELLOW and RED statuses.
+
+ The primary use of this method is to wait until a Jahia platform stabilizes after a startup or
+ provisioning operation.
+
+ Instead of waiting only for one occurence of a GREEN status, this function will wait until the a
+ GREEN status was returned a number of consecutive times (greenMatchCount).
+ */
+export const waitUntilSAMStatusGreen = (severity = 'MEDIUM', timeout = 60000, interval = 500, greenMatchCount = 10) : void => {
+    // We use YELLOW as the probeHealthFilter because we are not interested in potential GREEN probes in the response
+    waitUntilSAMStatus('GREEN', severity, 'YELLOW', timeout, interval, greenMatchCount);
 };
 
