e609434f3ac9c6f1111f306fd0a79e201581672d
Jonathan SINOVASSIN-NAIK <58434978+jsinovassin@users.noreply.github.com>
2024-11-25 11:54:29 +0100

    DMF-6379: add function to get modules version (#97)
    * DMF-6379: add function to get modules version
    
    * DMF-6379: utility function to get version of a dedicated module

--- src/utils/JahiaPlatformHelper.ts
+++ src/utils/JahiaPlatformHelper.ts
@@ -6,3 +6,18 @@ export const getJahiaVersion = (): Cypress.Chainable => {
         return result?.data?.admin.jahia.version;
     });
 };
+
+export const getStartedModulesVersion = (): Cypress.Chainable => {
+    return cy.apollo({
+        fetchPolicy: 'no-cache',
+        queryFile: 'graphql/jcr/query/getStartedModulesVersion.graphql'
+    }).then(result => {
+        return result?.data?.dashboard.modules;
+    });
+};
+
+export const getStartedModuleVersion = (moduleId: string): Cypress.Chainable => {
+    return getStartedModulesVersion().then(modules => {
+        return modules.find(module => module.id === moduleId)?.version;
+    });
+};
