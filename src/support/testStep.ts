46139cf116ec150ff382907d47651c605b08d3d4
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-04-16 17:55:07 +0200

    update linter rules

--- src/support/testStep.ts
+++ src/support/testStep.ts
@@ -5,6 +5,7 @@
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Cypress {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         interface Chainable<Subject> {
             /**
              * Creates a foldable test-case step with corresponding message in Cypress log
