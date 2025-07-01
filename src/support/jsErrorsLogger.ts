320be0a23e7ff28db1cbf377faf437b99e6f5d4b
Volodymyr Tereshchuk <vtereshchuk@jahia.com>
2025-07-01 17:46:24 +0200

    chore: refactor and simplify code

--- src/support/jsErrorsLogger.ts
+++ src/support/jsErrorsLogger.ts
@@ -8,63 +8,78 @@
 const envVarDisabled = 'JS_LOGGER_DISABLED';
 const envVarCollector = '__JS_LOGGER_FAILURES__';
 const envVarAllowedWarnings = '__JS_LOGGER_ALLOWED_WARNINGS__';
-const envVarHookFlag = '__JS_LOGGER_HOOK_ATTACHED__';
 const envVarStrategy = '__JS_LOGGER_STRATEGY__';
 
 /**
  * Strategy for handling JavaScript errors and warnings in Cypress tests.
- * - 'failFast': Fail *immediately* when an error is detected.
- * - 'failAfterEach': Collect all errors and warnings *during test* execution and fail if any issues are found.
- * - 'failAfterAll': Collect all errors and warnings *after all tests* and fail at the end of the test suite.
+ * - failFast: Fail *immediately* when an error is detected.
+ * - failAfterEach: Collect all errors and warnings *during test* execution and fail if any issues are found.
+ * - failAfterAll: Collect all errors and warnings *after all tests* and fail at the end of the test suite.
  */
-type Strategy = 'failFast' | 'failAfterAll' | 'failAfterEach';
+enum STRATEGY { failFast, failAfterAll, failAfterEach }
 
 /**
  * Returns the current strategy for handling JavaScript errors and warnings in Cypress tests.
- * @returns {Strategy} - The current strategy for handling JavaScript errors and warnings.
+ * @returns {STRATEGY} - The current strategy for handling JavaScript errors and warnings.
+ * @note be careful with Cypress.env(envVarStrategy), since it might return `0` for `failFast` strategy,
+ *       which is falsy in JavaScript, so we need to check if the variable is undefined.
  */
-const getStrategy = (): Strategy => {
-    return Cypress.env(envVarStrategy) || 'failFast';
-};
+function getStrategy(): STRATEGY {
+    return typeof Cypress.env(envVarStrategy) === 'undefined' ? STRATEGY.failFast : Cypress.env(envVarStrategy);
+}
 
 /**
  * Sets the strategy for handling JavaScript errors and warnings in Cypress tests.
- * @param {Strategy} strategy - Strategy for handling JavaScript errors and warnings.
+ * @param {STRATEGY} strategy - Strategy for handling JavaScript errors and warnings.
  * @throws {Error} If an invalid strategy is provided.
  * @returns {void}
  */
-const setStrategy = (strategy: Strategy): void => {
-    Cypress.env(envVarStrategy, strategy);
-};
+function setStrategy(strategy: STRATEGY): void { Cypress.env(envVarStrategy, strategy); }
 
 /**
  * Checks if the js errors and warnings logger is disabled.
  * @returns {boolean} - true if the logger is disabled, false otherwise.
  */
-const isDisabled = (): boolean => { return Cypress.env(envVarDisabled) === true; }
+function isDisabled(): boolean { return Cypress.env(envVarDisabled) === true; }
 
 /**
  * Sets the list of allowed warnings that will not be reported by the logger.
  * @param warnings {string[]} - Array of warning messages to be allowed.
  * @return {void}
  */
-const setAllowedJsWarnings = (warnings: string[]): void => { Cypress.env(envVarAllowedWarnings, warnings); };
+function setAllowedJsWarnings(warnings: string[]): void { Cypress.env(envVarAllowedWarnings, warnings); }
 
 /**
  * Attaches custom hooks to Cypress events to monitor and report JavaScript errors and warnings.
- * This method is called automatically when the logger is enabled.
+ * This method is called automatically in registerSupport.ts#registerSupport
  * It sets up listeners for console errors and warnings, collects them after each test,
  * and throws an error if any issues are found after all tests are executed.
  */
-const attachFailAfterAll = (): void => {
+function attachHook(): void {
     // Skip hook attachment if the logger is disabled (e.g. from CI/CD pipeline)
     if (isDisabled()) { return; }
 
-    // Prevent multiple hook attachments
-    if (Cypress.env(envVarHookFlag) === true) { return; }
-    // Set the flag to indicate that hooks are attached
-    Cypress.env(envVarHookFlag, true);
+    switch (getStrategy()) {
+        case STRATEGY.failAfterAll:
+            attachFailAfterAll();
+            break;
+        case STRATEGY.failAfterEach:
+            attachFailAfterEach();
+            break;
+        default:
+            attachFailFast();
+    }
+}
 
+/**
+ * Custom hook implementation which fails the test after all tests are executed if any JavaScript errors or warnings are found.
+ *
+ * Proc: Allows all tests to run, collects console errors and warnings, and fails the test suite at the end if any issues are found.
+ *       This is useful for reporting all issues at once after all tests are executed, rather than failing immediately on the first issue.
+ * Cons: Reporting might be confusing, e.g. - cypress will report the very last test as failed, while many tests might have issues.
+ *       This is because the hook is executed after all tests are completed, so the last test is reported as failed.
+ */
+function attachFailAfterAll(): void {
     /**
      * Custom 'window:before:load' hook to spy on console errors and warnings
      * This hook is executed before the page is loaded, allowing us to capture console messages.
@@ -127,9 +142,6 @@ const attachFailAfterAll = (): void => {
         // to skip js validations for some particular test and enable it afterward
         if (isDisabled()) { return; }
 
-        // Reset hook attachment flag
-        Cypress.env('__JS_LOGGER_HOOKS_ATTACHED__', false);
-
         // Analyze collected errors and warnings
         const failures = Cypress.env(envVarCollector) || [];
         cy.log('[JS ERRORS LOGGER] Analyze collected issues').then(() => {
@@ -145,10 +157,18 @@ const attachFailAfterAll = (): void => {
             }
         });
     });
-};
+}
 
-const attachFailAfterEach = (): void => {
-    // Skip hook attachment if the logger is disabled (e.g. from CI/CD pipeline)
+/**
+ * Custom hook implementation which fails the test after each test execution if any JavaScript errors or warnings are found.
+ *
+ * Proc: Allows each test to run, collects console errors and warnings, and fails the particular test by the end of it's execution
+ *       if any issues are found.
+ * Cons: If errors or warnings were found during beforeEach or afterEach hook(s), the rest of spec will be ignored.
+ */
+function attachFailAfterEach(): void {
+    // Double-check in case if functionality was disabled within the test-case code
+    // to skip js validations for some particular test and enable it afterward
     if (isDisabled()) { return; }
 
     /**
@@ -195,10 +215,18 @@ const attachFailAfterEach = (): void => {
             }
         });
     });
-};
+}
 
-const attachFailFast = (): void => {
-    // Skip hook attachment if the logger is disabled (e.g. from CI/CD pipeline)
+/**
+ * Custom hook implementation which fails the test immediately when a JavaScript error or warning is detected.
+ *
+ * Proc: Allows each test to run, looks for console errors and warnings, and fails the particular test IMMEDIATELLY
+ *       if any issues are found.
+ * Cons: If errors or warnings were found during beforeEach or afterEach hook(s), the rest of spec will be ignored.
+ */
+function attachFailFast(): void {
+    // Double-check in case if functionality was disabled within the test-case code
+    // to skip js validations for some particular test and enable it afterward
     if (isDisabled()) { return; }
 
     /**
@@ -211,8 +239,7 @@ const attachFailFast = (): void => {
     });
 
     Cypress.on('window:load', () => {
-        console.log('DOM FULLY LOADED AND PARSED. LOOKING FOR JS ISSUES...');
-
+        // DOM should be loaded and parsed by now, so we can check for console errors and warnings
         let consoleIssues = [];
         const allowedWarnings = Cypress.env(envVarAllowedWarnings) || [];
 
@@ -241,26 +268,14 @@ const attachFailFast = (): void => {
                 }
             });
     });
-};
-
-const attachHooks = (): void => {
-    switch (getStrategy()) {
-        case 'failAfterAll':
-            attachFailAfterAll();
-            break;
-        case 'failAfterEach':
-            attachFailAfterEach();
-            break;
-        default:
-            attachFailFast();
-    }
-};
+}
 
 /**
  * Exports the jsLogger module with methods to attach hooks, enable/disable logging, and set allowed warnings.
  */
 export const jsErrorsLogger = {
-    attachHooks,
+    attachHook,
     setAllowedJsWarnings,
-    setStrategy
+    setStrategy,
+    STRATEGY
 };
