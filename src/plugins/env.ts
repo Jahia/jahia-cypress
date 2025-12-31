269417f2538ab78a618e58d167ed21bb0cdb3309
Baptiste Grimaud <bgrimaud@jahia.com>
2025-12-31 01:09:47 +0100

    feat: Support optional JAHIA_PROCESSING_URL

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -3,15 +3,18 @@ const env = (on:Cypress.PluginEvents, config: Cypress.PluginConfigOptions):Cypre
         console.warn('No environment set, will use default values');
         config.baseUrl = 'http://localhost:8080';
         config.env.JAHIA_URL = 'http://localhost:8080';
+        config.env.JAHIA_PROCESSING_URL = 'http://localhost:8080';
         config.env.SUPER_USER_PASSWORD = 'root1234';
     } else {
         console.log('Setting environment');
         config.baseUrl = process.env.JAHIA_URL;
         config.env.JAHIA_URL = process.env.JAHIA_URL;
+        config.env.JAHIA_PROCESSING_URL = process.env.JAHIA_PROCESSING_URL;
         config.env.SUPER_USER_PASSWORD = process.env.SUPER_USER_PASSWORD;
     }
 
     console.log('JAHIA_URL =', config.env.JAHIA_URL);
+    console.log('JAHIA_PROCESSING_URL =', config.env.JAHIA_PROCESSING_URL);
     console.log('SUPER_USER_PASSWORD =', config.env.SUPER_USER_PASSWORD);
 
     return config;
