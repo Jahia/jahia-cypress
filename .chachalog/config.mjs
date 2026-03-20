c4fe04c891411bc3d9aa4670a037202edf423a95
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-03-20 10:01:23 +0100

    chore: setup chachalog with yarn manager

---
+++ .chachalog/config.mjs
@@ -0,0 +1,13 @@
+/// @ts-check
+/// <reference types="@chachalog/types" />
+import { defineConfig } from "chachalog";
+import github from "chachalog/github";
+import yarn from "chachalog/yarn";
+
+export default defineConfig(() => ({
+	allowedBumps: ["patch", "minor", "major"],
+	platform: github({
+		base: "main"
+	}),
+	managers: yarn()
+}));
