7e3ab1f3e8f071bc4a9379912af936caeadcfd5d
Francois G. <fgerthoffert@jahia.com>
2026-05-15 19:12:15 +0200

    Cleanup files

--- scaffolding/ci.build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./set-env.sh
-
-version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
-echo Using @jahia/cypress@$version...
-npx --yes --package @jahia/cypress@$version ci.build
--- scaffolding/ci.postrun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ./set-env.sh
-
-docker logs smtp-server > ./artifacts/results/smtp-server.log
