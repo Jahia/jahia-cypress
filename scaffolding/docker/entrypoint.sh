eb5d60cf44eacef89119f00735c78720e2b4f177
Francois G. <fgerthoffert@jahia.com>
2026-05-15 15:56:14 +0200

    Updated entrypoint

--- scaffolding/docker/entrypoint.sh
+++ scaffolding/docker/entrypoint.sh
@@ -1,58 +1,67 @@
 #!/bin/bash
-# This script can be used to warmup the environment and execute the tests
-# It is used by the docker image at startup
+source set-env.sh
 
-BASEDIR=$(dirname $(readlink -f $0))
-
-bash $BASEDIR/env.provision.sh
-
-source $BASEDIR/set-env.sh
-
-MODE=${MODE:-ci}
-
-echo "$(date +'%d %B %Y - %k:%M') == env.run.sh == Printing the most important environment variables"
-echo "$(date +'%d %B %Y - %k:%M') == NEXUS_USERNAME: ${NEXUS_USERNAME:0:3}***${NEXUS_USERNAME:(-6)}"
-echo "$(date +'%d %B %Y - %k:%M') == DEBUG: ${DEBUG}"
-echo "$(date +'%d %B %Y - %k:%M') == CYPRESS MODE: ${MODE}"
-
-if [[ "${DEBUG}" == "true" ]]; then
-  touch /tmp/debug
-fi
-
-echo "$(date +'%d %B %Y - %k:%M') == Fetching the list of installed modules =="
-bash -c "unset npm_config_package; npx --yes @jahia/jahia-reporter@latest utils:modules \
-  --moduleId=\"${MODULE_ID}\" \
-  --jahiaUrl=\"${JAHIA_URL}\" \
-  --jahiaPassword=\"${SUPER_USER_PASSWORD}\" \
-  --filepath=\"results/installed-jahia-modules.json\""
-echo "$(date +'%d %B %Y - %k:%M') == Modules fetched =="
-INSTALLED_MODULE_VERSION=$(cat results/installed-jahia-modules.json | jq '.module.version')
-if [[ $INSTALLED_MODULE_VERSION == "UNKNOWN" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: Unable to detect module: ${MODULE_ID} on the remote system "
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: The Script will exit"
-  echo "$(date +'%d %B %Y - %k:%M') ERROR: Tests will NOT run"
-  echo "failure" > ./results/test_failure
-  exit 1
-fi
-
-echo "$(date +'%d %B %Y - %k:%M') == Run tests =="
-mkdir -p ./results/reports
-rm -rf ./results/reports
-
-if [[ -z "${CYPRESS_CONFIGURATION_FILE}" ]]; then
-  CYPRESS_CONFIGURATION_FILE=cypress.config.ts
-fi
-
-if [[ "${TESTS_PROFILE}" != "" ]]; then
-  CYPRESS_CONFIGURATION_FILE=${TESTS_PROFILE}
+# Display various system details for debugging purposes
+echo "$(date +'%d %B %Y - %k:%M') == entrypoint.sh == Printing system details"
+echo "$(date +'%d %B %Y - %k:%M') Yarn version: $(yarn --version)"
+echo "$(date +'%d %B %Y - %k:%M') Node version: $(node --version)"
+echo "$(date +'%d %B %Y - %k:%M') Jahia CLI version: $(jahia-cli --version)"
+echo "$(date +'%d %B %Y - %k:%M') "
+echo "$(date +'%d %B %Y - %k:%M') == entrypoint.sh == Printing the most important environment variables"
+if [[ -n "${NEXUS_USERNAME}" ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') NEXUS_USERNAME: ${NEXUS_USERNAME:0:3}***${NEXUS_USERNAME:(-3)}"
 else
-  CYPRESS_CONFIGURATION_FILE="cypress.config.ts"
+  echo "$(date +'%d %B %Y - %k:%M') NEXUS_USERNAME: <empty>"
+fi
+echo "$(date +'%d %B %Y - %k:%M') DEBUG: ${DEBUG}"
+echo "$(date +'%d %B %Y - %k:%M') CYPRESS MODE: ${CYPRESS_MODE}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIACLI_CONFIG: ${JAHIACLI_CONFIG}"
+echo "$(date +'%d %B %Y - %k:%M') MANIFEST: ${MANIFEST}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_CLUSTER_ENABLED: ${JAHIA_CLUSTER_ENABLED}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_URL: ${JAHIA_URL}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_PROCESSING_URL: ${JAHIA_PROCESSING_URL}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_HOST: ${JAHIA_HOST}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_PORT: ${JAHIA_PORT}"
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_USERNAME: ${JAHIA_USERNAME}"
+if [[ -n "${JAHIA_PASSWORD}" ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD: ${JAHIA_PASSWORD:0:3}***${JAHIA_PASSWORD:(-3)}"
+else
+  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD: <empty>"
+fi
+echo "$(date +'%d %B %Y - %k:%M') JAHIA_USERNAME_TOOLS: ${JAHIA_USERNAME_TOOLS}"
+if [[ -n "${JAHIA_PASSWORD_TOOLS}" ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD_TOOLS: ${JAHIA_PASSWORD_TOOLS:0:3}***${JAHIA_PASSWORD_TOOLS:(-3)}"
+else
+  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD_TOOLS: <empty>"
+fi
+if [[ -n "${SUPER_USER_PASSWORD}" ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') SUPER_USER_PASSWORD: ${SUPER_USER_PASSWORD:0:3}***${SUPER_USER_PASSWORD:(-3)}"
+else
+  echo "$(date +'%d %B %Y - %k:%M') SUPER_USER_PASSWORD: <empty>"
+fi
+echo "$(date +'%d %B %Y - %k:%M') TIMEZONE: ${TIMEZONE}"
+
+# Provision the environment
+# This will execute a set of steps defined in a "provision" workflow named
+echo "$(date +'%d %B %Y - %k:%M') == entrypoint.sh == Executing Jahia CLI provision command =="
+jahia-cli workflow:run -c ${JAHIACLI_CONFIG} --name provision
+
+# Before running the tests, make sure there are no errors in the logs
+# In Server Availability Manager (SAM), a probe with LOW severity called JahiaErrorsProbe checks for errors in the logs.
+# Using it we can ensure there were no errors during the provisioning phase and that the environment is in a healthy state before running the tests.
+# If there are errors, print the logs and exit with failure to avoid running tests in a broken environment
+jahia-cli jahia:alive --timeout 300 --severity LOW
+if [[ $? -ne 0 ]]; then
+  echo "$(date +'%d %B %Y - %k:%M') == Errors detected in logs before running tests =="
+  echo "Printing the last 100 lines of the Jahia logs for debugging purposes:"
+  jahia-cli environment:logs --component jahia --tail 200
+else
+  echo "$(date +'%d %B %Y - %k:%M') == No errors detected in the Jahia logs, proceeding with tests =="
 fi
 
-echo "$(date +'%d %B %Y - %k:%M') == Running Cypress with configuration file ${CYPRESS_CONFIGURATION_FILE} =="
-
-NO_COLOR=1 yarn e2e:${MODE} --config-file "${CYPRESS_CONFIGURATION_FILE}"
-
+# Run the tests
+echo "$(date +'%d %B %Y - %k:%M') == Starting Cypress tests with mode: ${CYPRESS_MODE} and configuration file: ${CYPRESS_CONFIGURATION_FILE} =="
+NO_COLOR=1 yarn e2e:${CYPRESS_MODE} --config-file "${CYPRESS_CONFIGURATION_FILE}"
 if [[ $? -eq 0 ]]; then
   echo "$(date +'%d %B %Y - %k:%M') == Full execution successful =="
   echo "success" > ./results/test_success
@@ -63,6 +72,11 @@ else
   yarn report:merge; yarn report:html
 fi
 
+# Setting up the DEBUG flag
+# This flags keeps the environment running after tests have been running and until reaching the CI/CD timeout
+if [[ "${DEBUG}" == "true" ]]; then
+  touch /tmp/debug
+fi
 WAIT_DURATION=0
 while [[ -e /tmp/debug ]]; do
   echo "Debug file present - $(( ++ WAIT_DURATION ))s - waiting for file removal or expiration of GitHub Actions timeout..."
