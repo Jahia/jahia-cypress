917085821d15a17b3206302ed0841be893bb41d6
Francois G. <fgerthoffert@jahia.com>
2026-05-25 19:23:26 +0200

    Updated scaffolding

--- scaffolding/docker/entrypoint.sh
+++ scaffolding/docker/entrypoint.sh
@@ -8,44 +8,11 @@ echo "$(date +'%d %B %Y - %k:%M') Yarn version: $(yarn --version)"
 echo "$(date +'%d %B %Y - %k:%M') Node version: $(node --version)"
 echo "$(date +'%d %B %Y - %k:%M') Jahia CLI version: $(jahia-cli --version)"
 echo "$(date +'%d %B %Y - %k:%M') "
-echo "$(date +'%d %B %Y - %k:%M') == entrypoint.sh == Printing the most important environment variables"
-if [[ -n "${NEXUS_USERNAME}" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') NEXUS_USERNAME: ${NEXUS_USERNAME:0:3}***${NEXUS_USERNAME:(-3)}"
-else
-  echo "$(date +'%d %B %Y - %k:%M') NEXUS_USERNAME: <empty>"
-fi
-echo "$(date +'%d %B %Y - %k:%M') DEBUG: ${DEBUG}"
-echo "$(date +'%d %B %Y - %k:%M') CYPRESS MODE: ${CYPRESS_MODE}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIACLI_CONFIG: ${JAHIACLI_CONFIG}"
-echo "$(date +'%d %B %Y - %k:%M') MANIFEST: ${MANIFEST}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_CLUSTER_ENABLED: ${JAHIA_CLUSTER_ENABLED}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_URL: ${JAHIA_URL}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_PROCESSING_URL: ${JAHIA_PROCESSING_URL}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_HOST: ${JAHIA_HOST}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_PORT: ${JAHIA_PORT}"
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_USERNAME: ${JAHIA_USERNAME}"
-if [[ -n "${JAHIA_PASSWORD}" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD: ${JAHIA_PASSWORD:0:3}***${JAHIA_PASSWORD:(-3)}"
-else
-  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD: <empty>"
-fi
-echo "$(date +'%d %B %Y - %k:%M') JAHIA_USERNAME_TOOLS: ${JAHIA_USERNAME_TOOLS}"
-if [[ -n "${JAHIA_PASSWORD_TOOLS}" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD_TOOLS: ${JAHIA_PASSWORD_TOOLS:0:3}***${JAHIA_PASSWORD_TOOLS:(-3)}"
-else
-  echo "$(date +'%d %B %Y - %k:%M') JAHIA_PASSWORD_TOOLS: <empty>"
-fi
-if [[ -n "${SUPER_USER_PASSWORD}" ]]; then
-  echo "$(date +'%d %B %Y - %k:%M') SUPER_USER_PASSWORD: ${SUPER_USER_PASSWORD:0:3}***${SUPER_USER_PASSWORD:(-3)}"
-else
-  echo "$(date +'%d %B %Y - %k:%M') SUPER_USER_PASSWORD: <empty>"
-fi
-echo "$(date +'%d %B %Y - %k:%M') TIMEZONE: ${TIMEZONE}"
 
 # Provision the environment
 # This will execute a set of steps defined in a "provision" workflow named
 echo "$(date +'%d %B %Y - %k:%M') == entrypoint.sh == Executing Jahia CLI provision workflow =="
-jahia-cli workflow:run -c ${JAHIACLI_CONFIG} --name provision
+jahia-cli workflow:run -c ${JAHIACLI_CONFIG} --name provision --debug=${J_TESTS_DEBUG}
 
 # Before running the tests, make sure there are no errors in the logs
 # In Server Availability Manager (SAM), a probe with LOW severity called JahiaErrorsProbe checks for errors in the logs.
